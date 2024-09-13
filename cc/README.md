91fa064a4c122dd21760f3396153c8fff0efd053
SoJS coder <matfuller703@gmail.com>
2024-09-13 16:15:02 +0000

    added cookie clicker 2.052

---
+++ cc/README.md
@@ -0,0 +1,9 @@
+# Cookie-Clicker-Unblocked
+2.052 unblocked<br>
+Credits obviously go Orteil, visit the official website here: http://orteil.dashnet.org/cookieclicker/
+
+List of domains this is accessible.
+
+| Link                                     | Accessible w/ Lightspeed |
+|------------------------------------------|--------------------------|
+| https://ccported.github.io/CookieClicker | ✅                       |
---
+++ cc/showads.js
@@ -0,0 +1,8 @@
+//this lets us detect adblockers so we can adjust the layout in case ads aren't shown ! (we're not using this for anything weird, promise !)
+//this works because an adblocker will usually block this file from being embedded at all
+
+
+// Edit by sojs: setting this to false to prevent ads in general. 
+// I will roll a pre-game add each time the game loads and every, say, 30 minutes or so. All funds going directly to funding
+// large number of domains.
+var showAds=false;//true;
