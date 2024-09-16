60ca66ff456def801daf1a7d6e6a0b81b4418d5f
SoJS coder <matfuller703@gmail.com>
2024-09-16 16:33:23 +0000

    update tag script

--- gtag_events.js
+++ gtag_events.js
@@ -1,9 +1,10 @@
 const script = document.currentScript;
-
+alert("script registered")
 
 var gameID = script.getAttribute("data-gameID");
 
 function emit(){
+    alert(`logging event play ${gameID}, ${typeof gtag}`)
     gtag("event","play_game",{
         gameID,
         location: window.location.hostname
