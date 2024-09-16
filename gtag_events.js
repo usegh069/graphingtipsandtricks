1d9719fffe12dc7173b520eab917877b4fcaf0bf
SoJS coder <matfuller703@gmail.com>
2024-09-16 16:09:12 +0000

    updated game event script, renamed mrmine folder, moved tiny fishing to own url

---
+++ gtag_events.js
@@ -0,0 +1,13 @@
+const script = document.currentScript;
+
+
+var gameID = script.getAttribute("data-gameID");
+
+function emit(){
+    gtag("event","play_game",{
+        gameID,
+        location: window.location.hostname
+    });
+}
+emit();
+setInterval(emit, 1000 * 60 * 10);
