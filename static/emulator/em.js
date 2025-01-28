038f78d8839b1b7b3811689ad562755c7b2ea9a8
SoJS <matfuller703@gmail.com>
2025-01-27 20:31:13 -0800

    added additional emdata server, add idle dice

--- static/emulator/em.js
+++ static/emulator/em.js
@@ -33,7 +33,7 @@ async function init() {
     }
 }
 async function findDataCDN() {
-    var cdns = ["https://ccported.github.io/emdata", "https://emdata.onrender.com", "https://sojs-coder.github.io/emdata", "https://d0136284.ccportedemdata.pages.dev"];
+    var cdns = ["https://ccported.github.io/emdata", "https://emdata.onrender.com", "https://sojs-coder.github.io/emdata", "https://d0136284.ccportedemdata.pages.dev", "https://main.dtbdx4d2gvp5v.amplifyapp.com/"];
     for (var i = 0; i < cdns.length; i++) {
         try {
             const response = await fetch(cdns[i] + '/blocked_res.txt', {
---
+++ static/id/cordova.js
@@ -0,0 +1,1 @@
+console.log("CORDOVA.js")
