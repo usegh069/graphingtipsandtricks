baaa3f70d16a5389f1b66655a3e107b28771db0c
SoJS <matfuller703@gmail.com>
2025-03-19 21:14:25 -0700

    added profile dark theme

--- static/assets/scripts/lightDarkToggle.js
+++ static/assets/scripts/lightDarkToggle.js
@@ -2,7 +2,9 @@ const theme = localStorage.getItem("[ns_ccported]_theme") || "light";
 const lightDarkToggle = document.querySelector(".cc.lightDarkToggle .cc.checkbox");
 if(theme == "dark"){
     document.body.classList.add("dark");
+    lightDarkToggle.checked = true;
 }
 checkbox.addEventListener("change", (e) => {
     document.body.classList.toggle("dark");
+    localStorage.setItem("[ns_ccported]_theme", document.body.classList.contains("dark") ? "dark" : "light");
 })
